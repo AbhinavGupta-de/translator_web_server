38f703484a6618ab5487de8c476608a721c85abc
Abhinav Gupta <Abhinav.gupta@outlook.de>
2023-11-15 21:12:03 +0530

    fixed a few errors

--- fetchHTML.js
+++ fetchHTML.js
@@ -9,7 +9,14 @@ import cheerio from 'cheerio';
 async function fetchBodyFromUrl(url) {
 	try {
 		// Fetch the HTML content from the URL using axios.
-		const response = await axios.get(url);
+		const response = await axios.get(url, {
+			headers: {
+				'User-Agent':
+					'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+				'Access-Control-Allow-Origin': '*',
+				'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+			},
+		});
 
 		if (!response.data) {
 			throw new Error(`Failed to fetch the webpage. No data received.`);
